13710db193ee6a7d83b33065d259bb93cac602c8
Kaldyn2004 <kirillkropotov2016@gmail.com>
2025-02-20 00:18:47 +0300

    Добавил обработку данных приходящих из формы

--- Valuator/Pages/Index.cshtml.cs
+++ Valuator/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StackExchange.Redis;
 using System;
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Valuator.Pages;
 
@@ -22,25 +24,48 @@ public class IndexModel : PageModel
     }
 
     public IActionResult OnPost(string text)
+    {
+        _logger.LogDebug(text);
+
+        string id = Guid.NewGuid().ToString();
+
+        string textKey = "TEXT-" + id;
+        _redisDatabase.StringSet(textKey, text);
+
+        double similarity = CheckSimilarity(text);
+        _redisDatabase.StringSet(text, "1");
+
+        string rankKey = "RANK-" + id;
+        double rank = CalculateRank(text);
+        _redisDatabase.StringSet(rankKey, rank);
+
+        string similarityKey = "SIMILARITY-" + id;
+
+        _redisDatabase.StringSet(similarityKey, similarity);
+
+        return Redirect($"summary?id={id}");
+    }
+
+    private double CalculateRank(string text)
+    {
+        if (string.IsNullOrEmpty(text))
         {
-            _logger.LogDebug(text);
-
-            string id = Guid.NewGuid().ToString();
-
-            string textKey = "TEXT-" + id;
-            // Сохраняем текст в Redis
-            _redisDatabase.StringSet(textKey, text);
-
-            string rankKey = "RANK-" + id;
-            // Вычисляем ранг (пример: просто присваиваем значение 1, в реальности это может быть сложнее)
-            int rank = 1; // TODO: Замените на реальную логику вычисления ранга
-            _redisDatabase.StringSet(rankKey, rank);
-
-            string similarityKey = "SIMILARITY-" + id;
-            // Вычисляем сходство (пример: просто присваиваем значение 0.5, в реальности это может быть сложнее)
-            double similarity = 0.5; // TODO: Замените на реальную логику вычисления сходства
-            _redisDatabase.StringSet(similarityKey, similarity, TimeSpan.FromHours(24)); // Например, срок хранения 24 часа
-
-            return Redirect($"summary?id={id}");
+            return 0.0;
         }
+        var alphabetChars = Regex.Replace(text, @"[A-Za-zА-Яа-я]", "");
+
+        double rank = (double)alphabetChars.Length / text.Length;
+
+        return Math.Min(Math.Max(rank, 0.0), 1.0);
+    }
+
+    private double CheckSimilarity(string text)
+    {
+        bool keyExists = _redisDatabase.KeyExists(text);
+        if (keyExists)
+        {
+            return 1;
+        }
+        return 0.0;
+    }
 }
