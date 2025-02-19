2da85a3613a63aa8c85d3e3f4f230ca71b7028ee
Kaldyn2004 <kirillkropotov2016@gmail.com>
2025-02-19 23:22:12 +0300

    Добавил вывод значений из бд на страницу

--- Valuator/Pages/Summary.cshtml.cs
+++ Valuator/Pages/Summary.cshtml.cs
@@ -5,24 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace Valuator.Pages;
 public class SummaryModel : PageModel
 {
     private readonly ILogger<SummaryModel> _logger;
+    private readonly IDatabase _redisDatabase;
 
-    public SummaryModel(ILogger<SummaryModel> logger)
+    public SummaryModel(ILogger<SummaryModel> logger, IConnectionMultiplexer redisConnection)
     {
         _logger = logger;
+        _redisDatabase = redisConnection.GetDatabase();
     }
 
-    public double Rank { get; set; }
-    public double Similarity { get; set; }
+    public string RankStr { get; set; }
+    public string SimilarityStr { get; set; }
 
     public void OnGet(string id)
     {
-        _logger.LogDebug(id);
+        string rankKey = "RANK-" + id;
+        string similarityKey = "SIMILARITY-" + id;
+        RankStr = _redisDatabase.StringGet(rankKey);
+        SimilarityStr = _redisDatabase.StringGet(similarityKey);
 
-        // TODO: (pa1) проинициализировать свойства Rank и Similarity значениями из БД (Redis)
+        _logger.LogDebug(id);
     }
 }
